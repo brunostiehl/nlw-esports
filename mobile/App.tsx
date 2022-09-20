ecb008529fd22a524b97460d93a919938c529f95
Bruno <bruno@f1solucoes.com.br>
2022-09-20 03:39:54 -0300

    Alteradas as cores na aplicação mobile.

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text style={styles.helloText}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
@@ -12,9 +12,13 @@ export default function App() {
 
 const styles = StyleSheet.create({
   container: {
+    backgroundColor: '#281E5D',
     flex: 1,
-    backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
+  },
+  helloText: {
+    color: '#fff',
+    fontSize: 24,
   },
 });
